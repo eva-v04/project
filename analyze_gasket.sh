b14a02018d5af37b0d54116775b5f22c3326c4c0
Evanthia Violeti <sba2200006@uoa.gr>
2026-03-14 00:22:14 +0200

    Fixed errors in gasket shell script

--- analyze_gasket.sh
+++ analyze_gasket.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #import gasket
 
+#docker pull grgalex/gasket:0.1.0
+#docker run -ti --cap-add=SYS_PTRACE grgalex/gasket:0.1.0
+
 read package_name
 
 if [ -z "$package_name" ]; then
@@ -8,10 +11,17 @@ if [ -z "$package_name" ]; then
 	exit 1
 fi
 
-folder_name="static/gasket_analysis_$package_name"
+folder_name="$(pwd)/gasket_analysis_$package_name" #pwd Για να έχω full path
 mkdir -p "$folder_name"
-cd $folder_name
+chmod 777 "$folder_name"
 
-npm install --prefix . $package_name
+#npm install --prefix . $package_name
+#gasket -r ./node_modules/$package_name -o bridges_$package_name.json
 
-npx gasket -r $folder_name/node_modules/$package_name -o bridges_$package_name.json
+docker run --rm --cap-add=SYS_PTRACE \
+	    -v "$folder_name:/results" \
+	        grgalex/gasket:0.1.0 \
+		    /bin/bash -c "    #για να τρέξουν οι εντολές μέσα στο Container
+		    npm install $package_name && \
+		    gasket -r ./node_modules/$package_name -o /results/bridges_$package_name.json
+	    "
