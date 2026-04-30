7c3522c1fb3e3feb59843a4261c22eecce7b8491
Evanthia Violeti <sba2200006@uoa.gr>
2026-04-30 18:10:40 +0300

    updated caller info for native functions using FunctionInfo

--- jelly/src/analysis/fragmentstate.ts
+++ jelly/src/analysis/fragmentstate.ts
@@ -493,16 +493,34 @@ export class FragmentState {
      * Ignored if options.externalMatches is disabled.
      */
     registerEscapingToExternal(v: ConstraintVar | undefined, n: Node, encl: FunctionInfo | ModuleInfo) {
-        if (v && options.externalMatches) {
-            const callerLine = (encl && encl.loc) ? encl.loc.start.line : "top-level";
-            const exitPoint = n.loc ? n.loc.start.line : "unknown";
-            //DEBUG
-            logger.info(`DEBUG [EXTERNAL_ESCAPE] Data from line ${exitPoint} escapes to native code. Enclosing Caller starts at line: ${callerLine}`);
-            if (logger.isDebugEnabled())
-                logger.debug(`Values of ${v} escape to non-analyzed code at ${locationToStringWithFileAndEnd(n.loc)}`);
-            mapGetMap(this.maybeEscapingToExternal, v).set(n, encl);
+    if (v && options.externalMatches) {
+        const exitPoint = n.loc ? n.loc.start.line : "unknown";
+        const callerLine = (encl && encl.loc) ? encl.loc.start.line : "top-level";
+
+        // Αναγνώριση αν είναι Function ή Module
+        let callerDetails = "";
+        if (encl instanceof FunctionInfo) {
+            const funcName = encl.name || "<anonymous>";
+            const moduleName = encl.moduleInfo.toString();
+            const isConstructor = encl.isDummyConstructor ? "[Constructor]" : "";
+            
+            //  Όνομα, Module και τύπος
+            callerDetails = `Function: ${funcName} ${isConstructor} in ${moduleName}`;
+        } else {
+            callerDetails = `Module Level: ${encl.toString()}`;
         }
+
+        // Εκτύπωση εμπλουτισμένου DEBUG μηνύματος
+        logger.info(`DEBUG [EXTERNAL_ESCAPE]`);
+        logger.info(`  - Escape Point: line ${exitPoint}`);
+        logger.info(`  - Caller Info: ${callerDetails} (starts at line: ${callerLine})`);
+
+        if (logger.isDebugEnabled())
+            logger.debug(`Values of ${v} escape to non-analyzed code at ${locationToStringWithFileAndEnd(n.loc)}`);
+        
+        mapGetMap(this.maybeEscapingToExternal, v).set(n, encl);
     }
+}
 
     /**
      * Registers an expression that is invoked at a call.
